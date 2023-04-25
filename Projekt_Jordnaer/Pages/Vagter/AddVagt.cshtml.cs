7cd84da024e45347c678d38f7f64d5d37d40f7f7
Sarah <sarahenya2000@hotmail.dk>
2023-04-25 11:18:00 +0200

    Updated Vagt

--- Projekt_Jordnaer/Pages/Vagter/AddVagt.cshtml.cs
+++ Projekt_Jordnaer/Pages/Vagter/AddVagt.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Projekt_Jordnaer.Interfaces;
+using Projekt_Jordnaer.Models;
+using Projekt_Jordnaer.Services;
 
 namespace Projekt_Jordnaer.Pages.Vagter
 {
     public class AddVagtModel : PageModel
     {
+        public Vagt Vagt { get; set; }
+        private IVagtService vService;
+        public AddVagtModel(IVagtService vagtService)
+        {
+            vService = vagtService;
+        }
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await vService.CreateVagtAsync(Vagt);
+            return RedirectToPage("Index");
         }
     }
 }
--- Projekt_Jordnaer/Program.cs
+++ Projekt_Jordnaer/Program.cs
@@ -1,7 +1,12 @@
+using Projekt_Jordnaer.Interfaces;
+using Projekt_Jordnaer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IVagtService, VagtService>();
+
 
 var app = builder.Build();
 
